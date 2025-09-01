2ecf503c828d81c5174668e3935a8527b99005a8
bhr469@myy.haaga-helia.fi <bhr469@myy.haaga-helia.fi>
2025-09-01 16:00:52 +0300

    Add ja Delete

--- bookstore/src/main/java/com/example/bookstore/controller/BookController.java
+++ bookstore/src/main/java/com/example/bookstore/controller/BookController.java
@@ -3,6 +3,9 @@ package com.example.bookstore.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,4 +30,21 @@ public class BookController {
         return "booklist";
     }
 
+    @GetMapping("/addbook")
+    public String showAddBookFrom(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/addbook")
+    public String addBook(@ModelAttribute Book book) {
+        repository.save(book);
+        return "redirect:/booklist";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id) {
+        repository.deleteById(id);
+        return "redirect:/booklist";
+    }
 }
