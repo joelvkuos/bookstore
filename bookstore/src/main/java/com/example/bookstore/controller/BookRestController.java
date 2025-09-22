fb56e9faf16c13062f306c311aeba4ee203b9edd
bhr469@myy.haaga-helia.fi <bhr469@myy.haaga-helia.fi>
2025-09-22 13:04:05 +0300

    REST POST ja DELETE lisätty

--- bookstore/src/main/java/com/example/bookstore/controller/BookRestController.java
+++ bookstore/src/main/java/com/example/bookstore/controller/BookRestController.java
@@ -4,12 +4,16 @@ import com.example.bookstore.model.Book;
 import com.example.bookstore.model.BookRepository;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.http.HttpStatus;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/api")
@@ -31,4 +35,15 @@ public class BookRestController {
         return bookRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Book not found"));
     }
+
+    @PostMapping("/books")
+    public Book createBook(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
+    @DeleteMapping("/books/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+    }
+
 }
